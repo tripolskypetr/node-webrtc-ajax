007c39b8844ccad247f3f0ac9c9081f6c1bfbdf3
Petr Tripolsky <tripolskypetr@gmail.com>
2021-10-07 22:42:00 +0300

    docs

--- src/config/config/database.config.ts
+++ src/config/config/database.config.ts
@@ -9,7 +9,7 @@ export const databaseConfig = registerAs(DATABASE_CONFIG, (): IDatabase => ({
     host: process.env.DATABASE_HOST || 'localhost',
     port: +process.env.DATABASE_PORT || 5432,
     username: process.env.DATABASE_USERNAME || 'tripolskypetr',
-    password: process.env.DATABASE_PASSWORD || 'Halls20071',
+    password: process.env.DATABASE_PASSWORD || '12345678',
     database: process.env.DATABASE_DATABASE || 'postgres',
     synchronize: 'DATABASE_SYNCHRONIZE' in process.env ? !!+process.env.DATABASE_SYNCHRONIZE : true,
     autoLoadEntities: 'DATABASE_AUTO_LOAD_ENTITIES' in process.env ? !!+process.env.DATABASE_AUTO_LOAD_ENTITIES : true,
